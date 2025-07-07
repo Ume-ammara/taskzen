6add566d75772d0ba78962f93224ca500ea38c8c
Ume-ammara <aume9119@gmail.com>
2025-07-07 10:43:46 +0500

    update route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { registerUser, loginUser, verifiyUserEmail, resetPassword } from "../controllers/auth.controllers.js";
+import {
+  registerUser,
+  loginUser,
+  verifiyUserEmail,
+  resetPassword,
+  forgotPassword,
+} from "../controllers/auth.controllers.js";
 
-const router = Router()
-router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+const router = Router();
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
-router.route("/verify-email/:token").get(verifiyUserEmail)
-router.route("/reset-password/:resetToken").post(resetPassword)
+router.route("/verify-email/:token").get(verifiyUserEmail);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:token").post(resetPassword);
 
-export default router
+export default router;
