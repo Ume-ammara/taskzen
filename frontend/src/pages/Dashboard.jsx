cd33f7c7ad0237002f413c9c27771c230be6c221
Ume-ammara <amaraume24@gmail.com>
2025-09-15 13:07:33 +0500

    add vercel.json

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,8 +6,8 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className="w-screen h-screen flex flex-col"> 
-    <Navbar />  
+    <div className="w-screen h-screen flex flex-col">
+      <Navbar />
       <div className="flex flex-1">
         <Sidebar />
         <div className="flex-1">
